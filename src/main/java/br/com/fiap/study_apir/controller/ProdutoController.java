5a1ecf4fec3422199b1eea208268c02809ed70d8
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:48:55 -0300

    Atualiza ProdutoController e README

--- src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
@@ -1,45 +1,58 @@
 package br.com.fiap.study_apir.controller;
 
+import java.util.List;
+
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.study_apir.model.Produto;
+import br.com.fiap.study_apir.repository.RepositoryProdutoMockup;
+
 @RestController
-@RequestMapping("produtos")
+@RequestMapping("/api/${api.version}/produtos")
 public class ProdutoController {
+
+
+    private RepositoryProdutoMockup mockup = new RepositoryProdutoMockup();
+
 
     @PostMapping()
     public ResponseEntity<String> create(){
         return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado");
-        
         //return "Produto criado";
     }
 
-    @GetMapping
-    public ResponseEntity <String> findById(){
+    //codigo 200 => encontrado com sucesso
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
+    return ResponseEntity.status(HttpStatus.OK).body(mockup.findById(id));
+       
+    }
+    @GetMapping 
+    public ResponseEntity<List<Produto>>findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(mockup.findAll());
 
-        return ResponseEntity.status(HttpStatus.OK).body("jujubinha <3");
-        //return "jujubinha <3 ";
     }
 
     @PutMapping
-    public ResponseEntity <String> update(){
+    public ResponseEntity<String> update(){
         return ResponseEntity.status(HttpStatus.OK).body("Produto atualizado");
-
-
         //return "Produto atualizado";
     }
 
     @DeleteMapping
-    public ResponseEntity <String> delete(){
-
+    public ResponseEntity<String> delete(){
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Produto excluido");
         //return "Produto excluido";
     }
 }
+
+       
